1c2f4a85bb843fcd9623509a303a39bce8d28613
donavance08 <adonisavance@gmail.com>
2023-02-04 05:27:00 +0800

    finished course

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import { useState } from 'react';
-import StarRating from './components/StarRating';
+// import StarRating from './components/StarRating';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [status, setStatus] = useState(true);
+  const [name, setName] = useState('Dan');
+  useEffect(() => {
+    fetch('http://api.github.com/users')
+      .then((response) => response.json())
+      .then((result) => console.log);
+  });
 
   return (
     <>
-      <StarRating rating={10}/>
+      {/* <StarRating rating={10}/> */}
+      <section>
+        <p>Congratulations {name}!</p>
+        <button onClick={() => setName('Will')}>Change winner</button>
+      </section>
     </>
   );
 }
